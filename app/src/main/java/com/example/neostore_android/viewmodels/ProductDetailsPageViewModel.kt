971d41b6f5a2e556f9954bc06743d3cef3fd836b
Gokulnair21 <gokul.nairmurali@gmail.com>
2021-10-14 17:18:33 +0530

    Added/Seperated the ViewModel Of Product Details Page

---
+++ app/src/main/java/com/example/neostore_android/viewmodels/ProductDetailsPageViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.neostore_android.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.neostore_android.models.ProductRatingResponse
+import com.example.neostore_android.models.ProductResponse
+import com.example.neostore_android.repositories.ProductRepository
+import com.example.neostore_android.utils.NetworkData
+
+class ProductDetailsPageViewModel(private val productID: String) : ViewModel() {
+
+
+    private val productRepository = ProductRepository()
+
+    var product = MutableLiveData<NetworkData<ProductResponse>>()
+    var productRating = MutableLiveData<NetworkData<ProductRatingResponse>>()
+
+    init {
+        getProductDetails()
+    }
+
+    private fun getProductDetails() {
+        product = productRepository.getProductDetails(productID)
+    }
+
+    fun setProductRating(rating: Number) {
+        productRating = productRepository.setProductRating(productID, rating)
+    }
+
+    class Factory(private val productID: String) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return ProductDetailsPageViewModel(productID) as T
+        }
+    }
+}
