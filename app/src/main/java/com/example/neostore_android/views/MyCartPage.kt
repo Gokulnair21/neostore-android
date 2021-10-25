c54a000948eb5b22c787006a05f7196af2f70297
Gokulnair21 <gokul.nairmurali@gmail.com>
2021-10-25 13:10:10 +0530

    Added swipe to dismiss for cart page

--- app/src/main/java/com/example/neostore_android/views/MyCartPage.kt
+++ app/src/main/java/com/example/neostore_android/views/MyCartPage.kt
@@ -1,17 +1,81 @@
 package com.example.neostore_android.views
 
 
+import android.graphics.Canvas
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.neostore_android.R
 import com.example.neostore_android.adapters.MyCartListRecyclerViewAdapter
 import com.example.neostore_android.databinding.FragmentMyCartPageBinding
+import com.google.android.material.snackbar.Snackbar
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 
 class MyCartPage : BaseFragment<FragmentMyCartPageBinding>() {
 
 
     override fun setUpViews() {
         binding.myCartListsRecyclerView.adapter = MyCartListRecyclerViewAdapter()
+        ItemTouchHelper(itemTouchHelper).attachToRecyclerView(binding.myCartListsRecyclerView)
     }
+
+    private var itemTouchHelper =
+        object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                snackBar()
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                RecyclerViewSwipeDecorator.Builder(
+                    context,
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+                    .addSwipeLeftActionIcon(R.drawable.delete).setIconHorizontalMargin(15)
+                    .create()
+                    .decorate()
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+
+            }
+
+        }
+
+    private fun snackBar() {
+
+        Snackbar.make(requireView(), "Removed from the cart", Snackbar.LENGTH_SHORT)
+            .show()
+    }
+
 
     override fun getFragmentBinding(
         inflater: LayoutInflater,
