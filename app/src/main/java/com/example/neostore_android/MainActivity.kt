c2bc0da6e39f840d5724e0781d65e3090279fbc7
Gokulnair21 <gokul.nairmurali@gmail.com>
2021-11-08 11:41:51 +0530

    Implemented drawer notification

--- app/src/main/java/com/example/neostore_android/MainActivity.kt
+++ app/src/main/java/com/example/neostore_android/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.util.Base64
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Adapter
 import androidx.activity.viewModels
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.GravityCompat
@@ -19,8 +21,16 @@ import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.example.neostore_android.databinding.ActivityMainBinding
 import com.example.neostore_android.databinding.AppDrawerHeaderBinding
+import com.example.neostore_android.databinding.DrawerNotificationBinding
 import com.example.neostore_android.utils.NetworkData
 import com.example.neostore_android.viewmodels.AccountSharedViewModel
+import com.google.android.material.navigation.NavigationView
+import android.widget.Toast
+
+import androidx.annotation.NonNull
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,13 +60,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     @SuppressLint("SetTextI18n")
     private fun drawerDetails() {
+        val headerBinding = binding.drawerNavigationView.getHeaderView(0)
+        val drawerHeaderBinding = AppDrawerHeaderBinding.bind(headerBinding)
+        val drawerNotificationText = DrawerNotificationBinding.inflate(layoutInflater).root
+        binding.drawerNavigationView.menu.findItem(R.id.myCartPage).actionView = drawerNotificationText
         model.account.observe(this, { state ->
             if (state is NetworkData.Success) {
-                val headerBinding = binding.drawerNavigationView.getHeaderView(0)
-                val drawerHeaderBinding = AppDrawerHeaderBinding.bind(headerBinding)
                 state.data?.account?.let {
+                    drawerNotificationText.text = it.totalCarts.toString()
                     drawerHeaderBinding.emailID.text = it.user.email
                     drawerHeaderBinding.name.text = "${it.user.firstName} ${it.user.lastName}"
                     Glide.with(this)
@@ -111,5 +125,6 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
 
 }
